9fb0b058344b3b0f2d3033eeb981a36dfeb570d5
Mikhail's GIT <MikhailGOVENDER@gmail.com>
2022-05-09 08:57:55 +0200

    Created Entities

---
+++ ShoppingOnline.Solution/ShopOnline.API/Entities/CartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopOnline.API.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        //FK Reference to Cart -> 1 Cart Many CartItems
+        public int CartId { get; set; }
+        //FK Reference to Product -> 1 CartItem Many Products
+        public int ProductId { get; set; }
+        public int Qty { get; set; }
+    }
+}
---
+++ ShoppingOnline.Solution/ShopOnline.API/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ShopOnline.API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        //FK References to Category -> 1 Product Many Categories
+        public int CategoryId { get; set; }
+    }
+}
