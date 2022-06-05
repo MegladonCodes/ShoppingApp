fcad1f3bb17706930d3ac31ced5662fe05195922
Mikhail's GIT <MikhailGOVENDER@gmail.com>
2022-06-05 10:23:22 +0200

    Implemented Product Page that reads product data using the API

---
+++ ShoppingOnline.Solution/ShoppingOnline.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ShopOnline.Models.Dtos;
+
+namespace ShoppingOnline.Web.Services.Contracts
+{
+    /*
+     * Create services to wrap our API Call functionality
+     */
+    public interface IProductService
+    {
+        //Implement API Call method definitions
+        Task<IEnumerable<ProductDto>> GetItems();
+    }
+}
